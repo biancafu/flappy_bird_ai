a2400b95ddc7cca4ab93fc5d25682f6551522679
Bianca Fu <0509biancafu@gmail.com>
2023-02-27 16:49:14 -0700

    finish up the basic set up for game

--- flappy_bird.py
+++ flappy_bird.py
@@ -3,6 +3,7 @@ import neat
 import time
 import os
 import random
+pygame.font.init()
 
 # dimension of screen, constants
 WIN_WIDTH = 500
@@ -17,6 +18,8 @@ BIRD_IMGS = [
 PIPE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "pipe.png")))
 BASE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "base.png")))
 BG_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bg.png")))
+
+STAT_FONT = pygame.font.SysFont("comicsans", 35) #score font
 
 
 class Bird:
@@ -165,17 +168,28 @@ class Base:
 
 
 
-def draw_window(win, bird):
+def draw_window(win, bird, pipes, base, score):
     win.blit(BG_IMG, (0,0)) #position 0,0 (top left)
+    for pipe in pipes:  #can have multiple pipes (list of pipes) in screen
+        pipe.draw(win)
+    
+    #creating score
+    text = STAT_FONT.render("Score: " + str(score), 1,(255,255,255))
+    win.blit(text, (WIN_WIDTH - 10 - text.get_width(), 10)) #no matter how long the score is, will keep moving left to the screen
+    base.draw(win)
     bird.draw(win)
     pygame.display.update()
 
 
 #main loop of game
 def main():
-    bird = Bird(200, 200)
+    bird = Bird(230, 350)
+    base = Base(730) #730 is bottom of screen
+    pipes = [Pipe(600)]
     win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
     clock = pygame.time.Clock()
+
+    score = 0
 
     run = True
     while run:
@@ -183,8 +197,39 @@ def main():
         for event in pygame.event.get(): #listening for user event
             if event.type == pygame.QUIT:
                 run= False
-        bird.move()
-        draw_window(win, bird)
+
+        #bird.move()
+        add_pipe = False
+        rem = [] #removing pipes list
+        for pipe in pipes:
+            #collision test
+            if pipe.collide(bird): 
+                pass
+
+            #check if pipe is off the screen
+            if pipe.x + pipe.PIPE_TOP.get_width() < 0:
+                rem.append(pipe)
+            
+            #when bird passes pipe, we set passed = True (defined in bird class), and we need to add another pipe
+            if not pipe.passed and pipe.x < bird.x:
+                pipe.passed = True
+                add_pipe = True
+
+            pipe.move()
+        if add_pipe:
+            score += 1
+            pipes.append(Pipe(600))
+
+        #removing pipes off screen
+        for r in rem:
+            pipes.remove(r)
+
+        #dying condition (when it hits floor)
+        if bird.y + bird.img.get_height() >= 730: #730 is the floor/base
+            pass
+
+        base.move()
+        draw_window(win, bird, pipes, base, score)
     pygame.quit()
     quit()
 
